9aeba51ed664f30cf4b671ef0f05edf8388b0e4f
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-09-05 20:54:17 +0900

    change by patch

--- routers/students.js
+++ routers/students.js
@@ -1,18 +1,17 @@
 import express, { request, response } from "express";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 let students = [
   {
     id: "1",
     name: "sami",
-    degree: 70,
+    grede: 70,
   },
   {
     id: "2",
     name: "mjk",
-    degree: 89,
+    grede: 89,
   },
 ];
 router.get("/", (request, response) => {
@@ -29,15 +28,27 @@ router.get("/:id", (request, response) => {
 });
 router.delete("/:id", (request, response) => {
   const id = request.params.id;
-  students = students.filter((student) => student.id !== id)
-  response.send(`the student with ${id} has benn deleted`)
+  students = students.filter((student) => student.id !== id);
+  response.send(`the student with ${id} has benn deleted`);
 });
 
-router.post('/', (request, response) => {
+router.post("/", (request, response) => {
   const student = request.body;
-  const studentWithId={...student,id:uuidv4()}
-  students.push(studentWithId)
-  response.json(` the student has been ${student.request.name} addedd`)
-}) 
+  const studentWithId = { ...student, id: uuidv4() };
+  students.push(studentWithId);
+  response.json(` the student has been ${student.request.name} addedd`);
+});
 
+router.patch("/:id", (request, response) => {
+  id = request.params.id;
+  const { name, grede } = request.body;
+  let student = student.find((student) => student.id === id);
+  if (name) {
+    student.name = name;
+  }
+  if (grede) {
+    student.grede = grede;
+  }
+  response.send(`the student with ${id} has been updated`);
+});
 export default router;
